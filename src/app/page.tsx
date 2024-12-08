c1a35969a583718f3f2d8b345717e85e8b84c7a8
nehaltariq357 <143392985+nehaltariq357@users.noreply.github.com>
2024-12-08 17:04:47 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Banner from "./component/banner/page";
 import CompareDesign from "./component/compare-design/page";
 import NavBar from "./component/nav-bar/page";
+import Product from "./product/page";
 
 
 export default function Home() {
@@ -8,7 +9,8 @@ export default function Home() {
    <div>
     {/* <NavBar/> */}
     {/* <Banner/> */}
-    <CompareDesign/>
+    {/* <CompareDesign/> */}
+    <Product/>
    </div>
   );
 }
---
+++ src/app/product/page.tsx
@@ -0,0 +1,109 @@
+import React from "react";
+import Image from "next/image";
+import { popular_post,recomendation_car } from "../data/data";
+import { CiHeart } from "react-icons/ci";
+
+const Product = () => {
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold mb-6">Popular Cars</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        {popular_post.map((car) => (
+          <div key={car.id} className="bg-white shadow rounded-lg p-4">
+            <h2 className="text-lg font-bold flex items-center justify-between">
+              {car.title}{" "}
+              <span className="hover:bg-red-600 hover:text-white p-2 rounded-full cursor-pointer">
+                <CiHeart className="text-xl" />
+              </span>
+            </h2>
+            <span className="text-sm text-gray-500">{car.category}</span>
+            <Image
+              src={car.image}
+              alt={car.title}
+              width={500} // Adjust this to your desired width
+              height={300} // Adjust this to your desired height
+              layout="intrinsic" // Ensures the image scales correctly while maintaining aspect ratio
+              className="w-full h-auto rounded-md mb-4"
+            />
+
+            <div className="flex justify-between items-center mb-2"></div>
+            <div className="text-sm text-gray-500 mb-4">
+              <p>{car.gasoline}</p>
+              <p>{car.variant}</p>
+              <p>{car.capacity}</p>
+            </div>
+            <div className="flex justify-between items-center">
+              <div>
+              <span className="text-xl font-bold text-black">
+                  {car.price}/
+                </span><span className="text-slate-400">day</span>
+                {car.discountedPrice && (
+                  <span className="text-sm line-through text-gray-400 ml-2">
+                    {car.discountedPrice}
+                  </span>
+                )}
+              </div>
+              <button className="bg-blue-600 text-white px-4 py-2 rounded-md">
+                Rent Now
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* recomends car */}
+
+      <h1 className="text-2xl font-bold mb-6">Recomends Cars</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        {recomendation_car.map((car) => (
+          <div key={car.id} className="bg-white shadow rounded-lg p-4">
+            <h2 className="text-lg font-bold flex items-center justify-between">
+              {car.title}{" "}
+              <span className="hover:bg-red-600 hover:text-white p-2 rounded-full cursor-pointer">
+                <CiHeart className="text-xl" />
+              </span>
+            </h2>
+            <span className="text-sm text-gray-500">{car.category}</span>
+            <Image
+              src={car.image}
+              alt={car.title}
+              width={500} // Adjust this to your desired width
+              height={300} // Adjust this to your desired height
+              layout="intrinsic" // Ensures the image scales correctly while maintaining aspect ratio
+              className="w-full h-auto rounded-md mb-4"
+            />
+
+            <div className="flex justify-between items-center mb-2"></div>
+            <div className="text-sm text-gray-500 mb-4">
+              <p>{car.gasoline}</p>
+              <p>{car.variant}</p>
+              <p>{car.capacity}</p>
+            </div>
+            <div className="flex justify-between items-center">
+              <div>
+                <span className="text-xl font-bold text-black">
+                  {car.price}/
+                </span><span className="text-slate-400">day</span>
+                {car.discountedPrice && (
+                  <span className="text-sm line-through text-gray-400 ml-2">
+                    {car.discountedPrice}
+                  </span>
+                )}
+              </div>
+              <button className="bg-blue-600 text-white px-4 py-2 rounded-md">
+                Rent Now
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-center my-16">
+      <button className="bg-blue-600 text-white px-4 py-2 rounded-md">
+      Show more car
+              </button>
+      </div>
+    </main>
+  );
+};
+
+export default Product;
